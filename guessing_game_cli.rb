38b96ed6dd7a828510061b3211bf3ee30bb52723
Noah Scala <tobi8912@gmail.com>
2019-12-03 22:32:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,9 @@
 # Code your solution here!
+require 'pry'
 def run_guessing_game
   computer_guess = rand(6) + 1
   player_guess = gets.chomp
+  binding.pry
   if player_guess == computer_guess
     puts "You guessed the correct number!"
   elsif player_guess == "exit"
